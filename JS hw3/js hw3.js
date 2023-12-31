320534ed6f5accb03ab9a41717bf38b349b5bdd9
Якимов С <siakimov@list.ru>
2023-12-31 17:58:47 +0700

    js hw 4

--- JS hw3/js hw3.js
+++ JS hw3/js hw3.js
@@ -51,6 +51,7 @@ switch (monthNumber) {
   case 7:
   case 8:
     alert("Сейчас лето!")
+    break
   case 9:
   case 10:
   case 11:
---
+++ JS hw4/js hw4.js
@@ -0,0 +1,45 @@
+// Первое задание //
+for (let i = 1; i <= 2; i++) {
+  console.log("Привет");
+}
+
+// Второе задание //
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+//Третье задание//
+for (let i = 7; i >= 7 && i <= 22; i++) {
+  console.log(i);
+}
+
+//Четвёртое задание//
+const obj = {
+  Коля: 200,
+  Вася: 300,
+  Петя: 400,
+}  
+
+for (let item in obj){
+  console.log(`${item} - зарплата ${obj[item]} долларов.`);
+}
+
+//Пятое задание//
+let n = 1000
+let num = 0
+
+while (n >= 50) {
+  n /= 2
+  num ++ 
+}
+
+console.log(n);
+console.log(num);
+
+//Шестое задание//
+let firstFriday = 3
+
+for (let i = firstFriday; i <= 31; i += 7) {
+  console.log(`Сегодня пятница, ${i}-е число. Необходимо подготовить отчет.`); 
+}
+
